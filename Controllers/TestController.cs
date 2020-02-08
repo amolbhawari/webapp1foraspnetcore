8d7cd4440fa142080ae3cab4d4f8cd779f6c8975
Amol Bhawari <amol.bhawari@transfast.com>
2020-02-08 22:02:15 +0530

    New repo

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebApplication.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            return "Azure DevOps CICD - Version 5.2";
+
+        }
+    }
+}
